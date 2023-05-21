fa83399e140e464bb69254cb003ade987eed98c2
Luisina Martinez <luuumartinez13@gmail.com>
2023-05-20 21:31:13 -0300

    localstorage funcionando

--- js/usuarios.js
+++ js/usuarios.js
@@ -8,6 +8,146 @@ const apellidoUsuario = document.getElementById('apellidoUsu')
 const mailUsuario = document.getElementById('mailUsu');
 const claveUsuario = document.getElementById('claveUsu')
 const crearUsuario = document.getElementById('crearUsu');
+const parrafoNombre = document.querySelector('.parrafoNombre')
+const parrafoUsuario = document.querySelector('.parrafoUsuario')
+const parrafoMail = document.querySelector('.parrafoMail')
+const parrafoClave = document.querySelector('.parrafoClave')
+
+const inputs = document.querySelectorAll('#formRegistro input')
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{4,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{8,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+}
+
+const campos = {
+    nombreUsuario: false,
+    apellidoUsuario: false,
+    claveUsuario: false,
+    mailUsuario: false
+}
+
+const validarFormulario = (e) =>{
+    switch (e.target.name){
+        case "nombre":
+            if(expresiones.nombre.test(e.target.value)){
+                campos[nombreUsuario]
+                parrafoNombre.textContent = "parrafo correcto"
+            }else{
+                parrafoNombre.textContent = "parrafo incorrcto";
+            }
+            break;
+        case "apellido":
+             if(expresiones.usuario.test(e.target.value)){
+                campos[apellidoUsuario]
+                parrafoUsuario.textContent = "parrafo correcto"
+            }else{
+                parrafoUsuario.textContent = "parrafo incorrcto"
+            }
+            break;
+        case "email":
+            if(expresiones.correo.test(e.target.value)){
+                campos[mailUsuario]
+                parrafoMail.textContent = "parrafo correcto"
+            }else{
+                parrafoMail.textContent = "parrafo incorrcto"
+            }
+            break;
+        case "contrasena":
+            if(expresiones.password.test(e.target.value)){
+                campos[claveUsuario]
+                parrafoClave.textContent = "parrafo correcto"
+            }else{
+                parrafoClave.textContent = "parrafo incorrcto"
+            }
+            break;
+        }
+    
+}
+/*
+inputs.forEach((input)=>{
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario) 
+}) 
+crearUsuario.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    if (campos.nombre && campos.usuario && campos.correo && campos.password){
+        formRegistro.reset();
+    }
+})
+*/
+
+function uuidv4() {
+    return crypto.randomUUID();
+  };
+
+
+
+inputs.forEach((input)=>{
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+});
+
+//crearUsuario.addEventListener('click', (e) => {
+    //e.preventDefault();
+const enviarFormulario = () =>{
+    const nombre = nombreUsuario.value;
+    const apellido = apellidoUsuario.value;
+    const mail = mailUsuario.value;
+    const clave = claveUsuario.value;
+    const mode = agregarUsuario.dataset.mode;
+
+    if (mode === "add" && nombre !== ""  && apellido !== ""  && mail !== "" && clave !== ""){
+        const id = uuidv4();
+        const usuario = {id, nombre, apellido, mail};
+        usuarios.push(usuario);
+    } else if (mode === "delete"){
+       const index = usuarios.find((usuario) => usuario.id === id);
+       if (index !== -1){
+        usuarios.splice(index, 1);
+       }
+    }
+    //agregarUsuario.reset();
+    agregarUsuario.dataset.mode = "add";
+    mostrarUsuarios();
+};
+
+listaUsuarios.addEventListener("click", (e) => {
+    if (e.target.classList.contains("delete")){
+        const id = e.target.dataset.id;
+        const index = usuarios.findIndex((usuario) => usuario.id === id);
+        if (index !== -1){
+            usuarios.splice(index, 1);
+            mostrarUsuarios()
+        }
+    }
+})
+
+const mostrarUsuarios = () =>{
+    listaUsuarios.querySelector("tbody").innerHTML="";
+    usuarios.forEach((usuario) =>{
+        const tr = document.createElement("tr");
+        tr.innerHTML=`
+        <td>${usuario.nombre}</td>
+        <td>${usuario.apellido}</td>
+        <td>${usuario.mail}</td>
+        <td>
+        <button class="btn btn-danger delete" data-id="${usuario.id}">Eliminar</button>
+        </td>
+        `;
+        listaUsuarios.querySelector("tbody").appendChild(tr)
+    })
+    localStorage.setItem("usuarios", JSON.stringify(usuarios))
+}
+
+const usuariosLocalStorage = JSON.parse(localStorage.getItem("usuarios"));
+if (usuariosLocalStorage){
+    usuarios = usuariosLocalStorage;
+    mostrarUsuarios();
+};
+
 
 /*
 const validar = () => {
@@ -55,7 +195,7 @@ const esValido = (input, mensaje) =>{
 
 
 }
-*/
+
 
 const validar = () =>{
     const nombreValor = nombreUsuario.value;
@@ -70,65 +210,4 @@ const validar = () =>{
         esValido(nombreValor);
     }
 }
-
-function uuidv4() {
-    return crypto.randomUUID();
-  };
-
-
-crearUsuario.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const nombre = nombreUsuario.value;
-    const apellido = apellidoUsuario.value;
-    const mail = mailUsuario.value;
-    const mode = agregarUsuario.dataset.mode;
-
-    if (mode === "add"){
-        const id = uuidv4();
-        const usuario = {id, nombre, apellido, mail};
-        usuarios.push(usuario)
-    } else if (mode === "delete"){
-       const index = usuarios.find((usuario) => usuario.id === id);
-       if (index !== -1){
-        usuarios.splice(index, 1);
-       }
-    }
-    agregarUsuario.reset();
-    agregarUsuario.dataset.mode = "add";
-    mostrarUsuarios();
-})
-
-listaUsuarios.addEventListener("click", (e) => {
-    if (e.target.classList.contains("delete")){
-        const id = e.target.dataset.id;
-        const index = usuarios.findIndex((usuario) => usuario.id === id);
-        if (index !== -1){
-            usuarios.splice(index, 1);
-            mostrarUsuarios()
-        }
-    }
-})
-
-const mostrarUsuarios = () =>{
-    listaUsuarios.querySelector("tbody").innerHTML="";
-    usuarios.forEach((usuario) =>{
-        const tr = document.createElement("tr");
-        tr.innerHTML=`
-        <td>${usuario.nombre}</td>
-        <td>${usuario.apellido}</td>
-        <td>${usuario.mail}</td>
-        <td>
-        <button class="btn btn-danger delete" data-id="${usuario.id}">Eliminar</button>
-        </td>
-        `;
-        listaUsuarios.querySelector("tbody").appendChild(tr)
-    })
-    localStorage.setItem("usuarios", JSON.stringify(usuarios))
-}
-
-const usuariosLocalStorage = JSON.parse(localStorage.getItem("usuarios"));
-if (usuariosLocalStorage){
-    usuarios = usuariosLocalStorage;
-    mostrarUsuarios();
-};
+*/

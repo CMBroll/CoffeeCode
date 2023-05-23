c18ef69c295ce30d458d4400a3a65cc626004d94
Victoria Cavanna <viccavanna@gmail.com>
2023-05-23 00:35:09 -0300

    Se vincularon páginas y se hicieron modificaciones generales

--- js/login.js
+++ js/login.js
@@ -1,10 +1,74 @@
+let arrayUsuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+console.log(arrayUsuarios);
+
+const form = document.getElementById('form-login');
+const mailInput = document.getElementById('mailUsuario');
+const claveInput = document.getElementById('claveUsuario');
+const usuarioInvalido = document.getElementById("usuario-invalido");
+const iconLogin = document.getElementById("icon-login");
+
+mailInput.addEventListener('input', function() {
+  const valor = mailInput.value.trim();
+  if (arrayUsuarios.some(usuario => usuario.mail === valor)) {
+    mailInput.classList.remove('invalido');
+    mailInput.classList.add('valido');
+  } else {
+    mailInput.classList.remove('valido');
+    mailInput.classList.add('invalido');
+  }
+});
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const mail = mailInput.value;
+  const clave = claveInput.value;
+  const usuario = arrayUsuarios.find(usuario => usuario.mail === mail);
+  const admin = arrayUsuarios.find(usuario => usuario.mail.toLowerCase() === "admin@coco.com");
+    if (admin && clave === "AdminCoco"){
+    form.reset();
+    localStorage.setItem('usuarioActual', JSON.stringify(usuario));
+    window.location.href = "admin.html";
+    } else if (usuario && usuario.clave === clave) {
+      localStorage.setItem('usuarioActual', JSON.stringify(usuario));
+      console.log('Inicio de sesión exitoso');
+      iconLogin.classList.remove('fa-user');
+      iconLogin.classList.add('fa-right-from-bracket');
+      window.location.href = 'login.html';
+    } else {
+    // Credenciales inválidas
+    usuarioInvalido.style.display = "block";
+    console.log('Credenciales inválidas');
+  }
+});
 const inputs = document.querySelectorAll('.formItem input');
 inputs.forEach(input => {
-    input.addEventListener('input', () => {
-      if (input.value.length > 0) {
-        input.classList.add('hasValue');
-      } else {
-        input.classList.remove('hasValue');
-      }
-    });
+  input.addEventListener('input', () => {
+    if (input.value.length > 0) {
+      input.classList.add('hasValue');
+    } else {
+      input.classList.remove('hasValue');
+      input.classList.remove('invalido');
+      input.classList.remove('valido');
+      usuarioInvalido.style.display = "none";
+    }
+  });
 }); 
+
+const btnLogin = document.getElementById('btn-login');
+let usuarioActivo = localStorage.getItem('usuarioActual');
+if (usuarioActivo !== null && usuarioActivo !== undefined) {
+  let usuario = JSON.parse(usuarioActivo);
+  console.log("Hay un usuario actual:", usuario);
+  iconLogin.classList.remove('fa-user');
+  iconLogin.classList.add('fa-right-from-bracket');
+  
+  btnLogin.addEventListener('click', function() {
+    localStorage.removeItem('usuarioActual');
+    window.location.href = 'login.html';
+  });
+
+} else {
+  iconLogin.classList.add('fa-user');
+  iconLogin.classList.remove('fa-right-from-bracket');
+  console.log("No hay ningún usuario actual.");
+}
--- js/usuarios.js
+++ js/usuarios.js
@@ -1,134 +1,150 @@
 
+
 let usuarios = [];
 
-const listaUsuarios = document.getElementById('listaUsu');
-const agregarUsuario = document.getElementById('formRegistro');
-const nombreUsuario = document.getElementById('nombreUsu');
-const apellidoUsuario = document.getElementById('apellidoUsu')
-const mailUsuario = document.getElementById('mailUsu');
-const claveUsuario = document.getElementById('claveUsu')
-const crearUsuario = document.getElementById('crearUsu');
+const listaUsuarios = document.getElementById('listaUsuarios');
+const agregarUsuario = document.getElementById('form-registro');
+const nombreUsuario = document.getElementById('nombre');
+const apellidoUsuario = document.getElementById('apellido')
+const mailUsuario = document.getElementById('email');
+const claveUsuario = document.getElementById('contrasena')
+const crearUsuario = document.getElementById('crear-usuario');
+const parrafoNombre = document.querySelector('.parrafoNombre')
+const parrafoUsuario = document.querySelector('.parrafoUsuario')
+const parrafoMail = document.querySelector('.parrafoMail')
+const parrafoClave = document.querySelector('.parrafoClave')
 
-/*
-const validar = () => {
-    if(nombreUsuario.value === "" || apellidoUsuario.value === "" || mailUsuario.value === "" || claveUsuario.value === ""){
-        alert("El campo esta vacio");
-        return false;
-    }else if (nombreUsuario.value.length >20 || apellidoUsuario.value.length >20){
-        alert("muy largo");
-        return false;
-    }else if(mailUsuario.value.length >50 || ){
-        alert("muy largo");
-        return false;
-    }
+const inputs = document.querySelectorAll('#formRegistro input');
+
+const expresiones = {
+  usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+  nombre: /^[a-zA-ZÀ-ÿ\s]{4,40}$/, // Letras y espacios, pueden llevar acentos.
+  password: /^.{8,12}$/, // 4 a 12 digitos.
+  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 };
 
-formRegistro.addEventListener('click', (e) =>{
-    e.preventDefault();
-    
-    const validar = () =>{
-        const nombreValor = nombreUsuario.value;
-        const apellidoValor = apellidoUsuario.value;
-        const mailValor = mailUsuario.value;
-        const claveValor = claveUsuario.value;
-   
-        if (nombreValor === ""){
-            alert("campo vacio")
-            noValido(nombreValor, "campo vacio");
-        }else{
-            esValido(nombreValor);
-        }
-    }
-    validar();
-})
+const campos = {
+  nombreUsuario: false,
+  apellidoUsuario: false,
+  claveUsuario: false,
+  mailUsuario: false
+};
 
-const noValido = (input, mensaje) =>{
-    const formControl = input.parentElement;
-    const aviso = formControl.querySelector(".parrafo");
-    aviso.innerText = mensaje;
+const validarFormulario = (e) => {
+  switch (e.target.name) {
+    case "nombre":
+      if (expresiones.nombre.test(e.target.value)) {
+        campos['nombreUsuario'] = true;
+        parrafoNombre.textContent = "Párrafo correcto";
+      } else {
+        campos['nombreUsuario'] = false;
+        parrafoNombre.textContent = "Párrafo incorrecto";
+      }
+      break;
+    case "apellido":
+      if (expresiones.usuario.test(e.target.value)) {
+        campos['apellidoUsuario'] = true;
+        parrafoUsuario.textContent = "Párrafo correcto";
+      } else {
+        campos['apellidoUsuario'] = false;
+        parrafoUsuario.textContent = "Párrafo incorrecto";
+      }
+      break;
+    case "email":
+      if (expresiones.correo.test(e.target.value)) {
+        campos['mailUsuario'] = true;
+        parrafoMail.textContent = "Párrafo correcto";
+      } else {
+        campos['mailUsuario'] = false;
+        parrafoMail.textContent = "Párrafo incorrecto";
+      }
+      break;
+    case "contrasena":
+      if (expresiones.password.test(e.target.value)) {
+        campos['claveUsuario'] = true;
+        parrafoClave.textContent = "Párrafo correcto";
+      } else {
+        campos['claveUsuario'] = false;
+        parrafoClave.textContent = "Párrafo incorrecto";
+      }
+      break;
+  }
+};
 
-    
-}
-
-const esValido = (input, mensaje) =>{
-    const formControl = input.parentElement;
-
-
-}
-*/
-
-const validar = () =>{
-    const nombreValor = nombreUsuario.value;
-    const apellidoValor = apellidoUsuario.value;
-    const mailValor = mailUsuario.value;
-    const claveValor = claveUsuario.value;
-
-    if (nombreValor === ""){
-        alert("campo vacio")
-        noValido(nombreValor, "campo vacio");
-    }else{
-        esValido(nombreValor);
-    }
-}
+inputs.forEach((input) => {
+  input.addEventListener('keyup', validarFormulario);
+  input.addEventListener('blur', validarFormulario);
+});
 
 function uuidv4() {
     return crypto.randomUUID();
   };
-
-
-crearUsuario.addEventListener('click', (e) => {
-    e.preventDefault();
-
+  // const admin = {
+  //   id: "000",
+  //   nombre: "Coco",
+  //   apellido: "admin",
+  //   mail: "admin@coco.com",
+  //   clave: "AdminCoco",
+  //   mode: "add"
+  // };
+  // usuarios.push(usuario);
+const enviarFormulario = () => {
     const nombre = nombreUsuario.value;
     const apellido = apellidoUsuario.value;
     const mail = mailUsuario.value;
+    const clave = claveUsuario.value;
     const mode = agregarUsuario.dataset.mode;
-
-    if (mode === "add"){
-        const id = uuidv4();
-        const usuario = {id, nombre, apellido, mail};
-        usuarios.push(usuario)
-    } else if (mode === "delete"){
-       const index = usuarios.find((usuario) => usuario.id === id);
-       if (index !== -1){
+  
+    if (mode === "add" && nombre !== "" && apellido !== "" && mail !== "" && clave !== "") {
+      const id = uuidv4();
+      const usuario = { id, nombre, apellido, mail, clave};
+      usuarios.push(usuario);
+    } else if (mode === "delete") {
+      const id = agregarUsuario.dataset.id;
+      const index = usuarios.findIndex((usuario) => usuario.id === id);
+      if (index !== -1) {
         usuarios.splice(index, 1);
-       }
+      }
     }
+  
     agregarUsuario.reset();
     agregarUsuario.dataset.mode = "add";
     mostrarUsuarios();
-})
+};
+  
 
+crearUsuario.addEventListener('click', enviarFormulario);
+console.log("Usuario creado")
 listaUsuarios.addEventListener("click", (e) => {
-    if (e.target.classList.contains("delete")){
-        const id = e.target.dataset.id;
-        const index = usuarios.findIndex((usuario) => usuario.id === id);
-        if (index !== -1){
-            usuarios.splice(index, 1);
-            mostrarUsuarios()
-        }
+  if (e.target.classList.contains("delete")) {
+    const id = e.target.dataset.id;
+    const index = usuarios.findIndex((usuario) => usuario.id === id);
+    if (index !== -1) {
+      usuarios.splice(index, 1);
+      mostrarUsuarios();
     }
-})
+  }
+});
 
-const mostrarUsuarios = () =>{
-    listaUsuarios.querySelector("tbody").innerHTML="";
-    usuarios.forEach((usuario) =>{
-        const tr = document.createElement("tr");
-        tr.innerHTML=`
-        <td>${usuario.nombre}</td>
-        <td>${usuario.apellido}</td>
-        <td>${usuario.mail}</td>
-        <td>
+const mostrarUsuarios = () => {
+  listaUsuarios.querySelector("tbody").innerHTML = "";
+  usuarios.forEach((usuario) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${usuario.nombre}</td>
+      <td>${usuario.apellido}</td>
+      <td>${usuario.mail}</td>
+      <td>
         <button class="btn btn-danger delete" data-id="${usuario.id}">Eliminar</button>
-        </td>
-        `;
-        listaUsuarios.querySelector("tbody").appendChild(tr)
-    })
-    localStorage.setItem("usuarios", JSON.stringify(usuarios))
-}
+      </td>
+    `;
+    listaUsuarios.querySelector("tbody").appendChild(tr);
+  });
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+};
 
 const usuariosLocalStorage = JSON.parse(localStorage.getItem("usuarios"));
-if (usuariosLocalStorage){
-    usuarios = usuariosLocalStorage;
-    mostrarUsuarios();
+if (usuariosLocalStorage) {
+  usuarios = usuariosLocalStorage;
+  mostrarUsuarios();
 };
